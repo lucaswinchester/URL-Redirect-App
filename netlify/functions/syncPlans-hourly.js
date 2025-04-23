2783bd7f0b14bcb094b9eeaf10c8e8987ad4f31b
Lucas Winchester <lwinchester@revgennetworks.com>
2025-04-23 14:29:37 -0500

    sync addons

--- netlify/functions/syncPlans-hourly.js
+++ netlify/functions/syncPlans-hourly.js
@@ -1,2 +1,15 @@
-const { handler } = require('./syncPlans');
-exports.handler = handler;
+const { handler: syncPlansHandler } = require('./syncPlans');
+const { handler: syncAddonsHandler } = require('./syncAddons');
+
+exports.handler = async (event, context) => {
+  const plansResult = await syncPlansHandler(event, context);
+  const addonsResult = await syncAddonsHandler(event, context);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      plans: JSON.parse(plansResult.body),
+      addons: JSON.parse(addonsResult.body)
+    })
+  };
+};
