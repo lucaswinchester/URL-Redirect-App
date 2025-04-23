a00626b5994f039f10863798071bcb344e7f671c
Lucas Winchester <lwinchester@revgennetworks.com>
2025-04-23 15:05:18 -0500

    public sb key

--- public/js/supabaseClient.js
+++ public/js/supabaseClient.js
@@ -1,8 +1,7 @@
 // /public/js/supabaseClient.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const SUPABASE_URL = 'https://bxwhobiazcaijsyxcogv.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4d2hvYmlhemNhaWpzeXhjb2d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5MDQ4ODEsImV4cCI6MjA2MDQ4MDg4MX0.ed7whMpzl7YQxpNFl_c65bK6oCGWRzgVdUuthtlqOmw';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
-1
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
