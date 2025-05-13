52e28cb1e634d8ffaace82dbbdf468fc9594bd31
Lucas Winchester <lwinchester@revgennetworks.com>
2025-05-13 10:18:35 -0500

    updated api call

--- netlify/functions/getZohoLink.js
+++ netlify/functions/getZohoLink.js
@@ -7,6 +7,57 @@ const ZOHO_BILLING_API_URL = "https://www.zohoapis.com/billing/v1";
 async function createZohoPaymentLink(planID, agentInfo) {
   const accessToken = await getZohoAccessToken();
   
+  // Prepare billing address
+  const billingAddress = agentInfo.billing_address ? {
+    attention: agentInfo.billing_address.attention || '',
+    street: agentInfo.billing_address.street || '',
+    city: agentInfo.billing_address.city || '',
+    state: agentInfo.billing_address.state || '',
+    zip: agentInfo.billing_address.zip || '',
+    country: agentInfo.billing_address.country || 'US',
+    fax: agentInfo.billing_address.fax || ''
+  } : null;
+
+  // Prepare shipping address (use billing if shipping is same as billing)
+  const shippingAddress = agentInfo.shipping_address ? {
+    attention: agentInfo.shipping_address.attention || '',
+    street: agentInfo.shipping_address.street || '',
+    city: agentInfo.shipping_address.city || '',
+    state: agentInfo.shipping_address.state || '',
+    zip: agentInfo.shipping_address.zip || '',
+    country: agentInfo.shipping_address.country || 'US',
+    fax: agentInfo.shipping_address.fax || ''
+  } : billingAddress;
+  
+  const requestBody = {
+    customer: {
+      first_name: agentInfo["First Name"] || "",
+      last_name: agentInfo["Last Name"] || "",
+      display_name: `${agentInfo["First Name"] || ''} ${agentInfo["Last Name"] || ''}`.trim(),
+      email: agentInfo["Email"],
+      company_name: agentInfo["Company Name"] || '',
+      billing_address: billingAddress,
+      shipping_address: shippingAddress
+    },
+    plan: {
+      plan_code: planID
+    },
+    addons: [],
+    custom_fields: [
+      {
+        label: "Agent ID#",
+        value: agentInfo["Agent ID"] || ''
+      },
+      {
+        label: "Dealer ID#",
+        value: agentInfo["Dealer ID"] || ''
+      }
+    ],
+    redirect_url: process.env.SUCCESS_REDIRECT_URL,
+  };
+
+  console.log('Sending to Zoho:', JSON.stringify(requestBody, null, 2));
+  
   const options = {
     method: 'POST',
     headers: {
@@ -14,28 +65,8 @@ async function createZohoPaymentLink(planID, agentInfo) {
       'Authorization': `Zoho-oauthtoken ${accessToken}`,
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({
-      "customer": {
-        "first_name": agentInfo["First Name"] || "",
-        "last_name": agentInfo["Last Name"] || "",
-        "display_name": `${agentInfo["First Name"] || ''} ${agentInfo["Last Name"] || ''}`.trim(),
-        "email": agentInfo["Email"],
-        "company_name": agentInfo["Company Name"]
-      },
-      "plan": {
-        "plan_code": planID
-      },
-      "addons": [],
-      "custom_fields": [{
-        "label": "Agent ID#",
-        "value": agentInfo["Agent ID"]
-      }, {
-        "label": "Dealer ID#",
-        "value": agentInfo["Dealer ID"]
-      }],
-      "redirect_url": process.env.SUCCESS_REDIRECT_URL,
-    })
-    };
+    body: JSON.stringify(requestBody)
+  };
 
     const response = await fetch(`${ZOHO_BILLING_API_URL}/hostedpages/newsubscription`, options);
     const data = await response.json();
